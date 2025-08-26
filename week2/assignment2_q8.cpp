5f33f9f052b58c4197c2248effdc3b42a97ea12a
Gurkirat Singh Dhaliwal <gurkiratsingh272006@gmail.com>
2025-08-27 00:48:39 +0530

    Create week2/assignment2_q8.cpp

---
+++ week2/assignment2_q8.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int arr[] = {1, 2, 3, 2, 4, 1, 5};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int distinctCount = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        bool isDuplicate = false;
+        for (int j = 0; j < i; j++)
+        {
+            if (arr[i] == arr[j])
+            {
+                isDuplicate = true;
+                break;
+            }
+        }
+        if (!isDuplicate)
+        {
+            distinctCount++;
+        }
+    }
+
+    cout << "Total number of distinct elements: " << distinctCount << endl;
+    return 0;
+}
