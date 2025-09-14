d1c8296ed3dd7968d2355354f3e216e43e1aec82
Gurkirat Singh Dhaliwal <gurkiratsingh272006@gmail.com>
2025-09-14 23:29:23 +0530

    Update assignment5_q3.cpp

--- week5/assignment5_q3.cpp
+++ week5/assignment5_q3.cpp
@@ -1,1 +1,47 @@
+#include <iostream>
+using namespace std;
 
+struct N
+{
+    int d;
+    N *n;
+    N(int x) : d(x), n(NULL) {}
+};
+
+void ins(N *&h, int v)
+{
+    if (!h)
+        h = new N(v);
+    else
+    {
+        N *t = h;
+        while (t->n)
+            t = t->n;
+        t->n = new N(v);
+    }
+}
+
+int main()
+{
+    N *H = 0;
+    int x;
+    cout << "Enter list vals (-1 to stop): ";
+    while (cin >> x && x != -1)
+        ins(H, x);
+
+    if (!H)
+    {
+        cout << "Emtpy list\n";
+        return 0;
+    }
+
+    N *s = H, *f = H;
+    while (f && f->n)
+    {
+        s = s->n;
+        f = f->n->n;
+    }
+
+    cout << "Middle: " << s->d << endl;
+    return 0;
+}
